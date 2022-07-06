cd53792ba1041bb894d77bac447868bd60c6d6d7
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-07-06 00:23:07 -0400

    FlatList done and Header

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CarItem from './components/CarItem';
+import { React } from "react";
+import { StyleSheet, View } from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-
-        <CarItem />
-
+        <Header/>
+        <CarsList/>
       <StatusBar style="auto" />
     </View>
   );
