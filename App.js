9845794795f87733cdacceb0a21bd4c24c4abfcf
Andrey Yuschenko <yuschenkoandrey.work@gmail.com>
2022-07-05 22:10:24 -0400

    Created component CarItem

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style = {styles.carContainer}>
 
-
-        <View style = {styles.titles}>
-            <Text style = {styles.title}>Model S</Text>
-            <Text style = {styles.subtitle}>Starting at $69,420</Text>
-        </View>
-
-
-
-      </View>
-
+        <CarItem />
 
       <StatusBar style="auto" />
     </View>
@@ -29,21 +20,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  carContainer:{
-    width: '100%',
-    height: '100%',
-  },
-  titles:{
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center',
-  },
-  title:{
-    fontSize: 40,
-    fontWeight: '500',
-  },
-  subtitle:{
-    fontSize: 16,
-    color: "#5c5e62"
-  }
+
 });
